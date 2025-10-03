9ef7a389069279c359db23029468c618783a07c4
kamstand <kh84590@gmail.com>
2025-10-03 18:40:20 -0400

    display expr and unittest

--- expr.d
+++ expr.d
@@ -35,7 +35,7 @@ Expression* makeLiteral(Token value)
 {
     Literal* lit = new Literal(value);
     Expression* expr = new Expression();
-    expr.ExprType = ExprType.Literal;
+    expr.type = ExprType.Literal;
     expr.lit = lit;
     return expr;
 
@@ -46,7 +46,46 @@ Expression* makeBinary(Expression* lhs, Token op, Expression* rhs)
 
     Binary* bin = new Binary(lhs, op, rhs);
     Expression* expr = new Expression();
-    expr.ExprType = ExprType.Binary;
+    expr.type = ExprType.Binary;
     expr.bin = bin;
     return expr;
+}
+
+void display(Expression* expr)
+{
+
+    switch (expr.type)
+    {
+    case ExprType.Literal:
+        writefln("\tLITERAL: [TYPE: %s] | [VAL: %c]", expr.lit.value.type, expr.lit.value.ch);
+        break;
+    case ExprType.Binary:
+        writeln("BINARY: ");
+        display(expr.bin.lhs);
+        writefln("\tOPERATION: [TYPE: %s] | [VAL: %c]", expr.bin.op.type, expr.bin.op.ch);
+        display(expr.bin.rhs);
+        break;
+    default:
+        break;
+    }
+}
+
+unittest
+{
+    auto lit = makeLiteral(Token(TokenType.Atom, '1'));
+    assert(lit.type == ExprType.Literal);
+}
+
+unittest
+{
+    Expression* lhs = makeLiteral(Token(TokenType.Atom, '1'));
+    Expression* rhs = makeLiteral(Token(TokenType.Atom, '2'));
+    Token op = Token(TokenType.Op, '+');
+
+    Expression* bin = makeBinary(lhs, op, rhs);
+    assert(bin.type == ExprType.Binary);
+    assert(bin.bin.lhs.lit.value.ch == '1');
+
+    display(bin);
+
 }
